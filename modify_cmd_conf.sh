caaec23a46e6b3321b51a80b72e05ac173d8df60
Maopu Xu <xumaopu233@gmail.com>
2025-08-26 14:41:05 -0500

    bug fix during modifying image index

--- modify_cmd_conf.sh
+++ modify_cmd_conf.sh
@@ -7,4 +7,4 @@ FILE_ID="$3"
 ESC_PATH=$(echo "$NEW_PATH" | sed 's/[&/\]/\\&/g')
 #echo $ESC_PATH
 sed -i "s/'[^']*\.project/'$ESC_PATH/g" "$CONF_FILE"
-sed -i "s/project',\d+,/project',$FILE_ID,/1" "$CONF_FILE"
+sed -Ei "s/',[0-9]+,/',$FILE_ID,/1" "$CONF_FILE"
